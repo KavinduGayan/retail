6c8789c6b155d1693fb1b2265d1e34d721223161
kavindu <kavindug@hsenidmobile.com>
2023-11-09 11:13:15 +0530

    initial commit

---
+++ customer-service/src/main/kotlin/com/example/retail/CustomerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.retail
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CustomerApplication
+
+fun main(args: Array<String>) {
+    runApplication<CustomerApplication>(*args)
+}
---
+++ src/main/kotlin/com/example/retail/RetailApplication.kt
@@ -0,0 +1,11 @@
+package com.example.retail
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RetailApplication
+
+fun main(args: Array<String>) {
+    runApplication<RetailApplication>(*args)
+}
